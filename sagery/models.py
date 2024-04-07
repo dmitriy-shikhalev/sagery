e1ebf29e99efe5cdc8f38fae6a8c0aefdf2e304d
Dmitriy Shikhalev <d.shikhalev@innopolis.ru>
2024-04-07 14:32:30 +0500

    change in models Request

--- sagery/models.py
+++ sagery/models.py
@@ -81,7 +81,7 @@ class Request(Base):
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
     job_id: Mapped[int] = mapped_column(ForeignKey("job.id"))
     job: Mapped["Job"] = relationship(back_populates="requests")
-    operator_name: Mapped[int] = mapped_column(ForeignKey("operator.name"))
+    operator_name: Mapped[str] = mapped_column(ForeignKey("operator.name"))
     operator: Mapped["Operator"] = relationship(back_populates="requests")
     inputs: Mapped[list["Input"]] = relationship(back_populates="request")
     outputs: Mapped[list["Output"]] = relationship(back_populates="request")
