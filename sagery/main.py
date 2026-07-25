9c340abf41f727b00d3c32698b5809565d2d18ce
Dmitriy Shikhalev <d.shihalev@ntechlab.com>
2026-07-25 09:38:23 +0500

    ...

---
+++ sagery/main.py
@@ -0,0 +1,24 @@
+import asyncio
+import sys
+
+
+async def async_sum_nums(args):
+    return sum(map(int, args))
+
+
+operators = {
+    "sum": async_sum_nums,
+}
+
+
+def main():
+    if len(sys.argv) < 2:
+        raise RuntimeError("Use: `sagery sum 1 2`")
+    else:
+        operator_name = sys.argv[1]
+        operator = operators.get(operator_name)
+        if operator is None:
+            raise RuntimeError(f"There is no operator `{operator_name}`")
+
+        result = asyncio.run(operator(sys.argv[2:]))
+        print(f"Result is {result}")
