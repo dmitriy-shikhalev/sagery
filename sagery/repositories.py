e5d3d5c97a483a62cf22f20fd4bcf56f607ac70d
Dmitriy Shikhalev <d.shihalev@ntechlab.com>
2026-09-25 21:13:55 +0500

    update repositories.py

--- sagery/repositories.py
+++ sagery/repositories.py
@@ -5,28 +5,41 @@ from typing import Any
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from sagery import domain
-from sagery.models import Input, Job, Launch, Operator, Output, Queue, Saga, Stream, Value
+from sagery.converters import AbstractConverter, SagaConverter
+from sagery.models import Saga
+
+# from sagery.models import Input, Job, Launch, Operator, Output, Queue, Saga, Stream, Value
 from sagery.types import DBModel, DomainModel
 
 
-class AbstractRepository[db_model_type: type[DBModel], domain_model_type: type[DomainModel]](ABC):
+class AbstractRepository[db_model_type: DBModel, domain_model_type: DomainModel, converter_class: AbstractConverter](
+    ABC
+):
     def __init__(self, session: AsyncSession):
         self.session = session
 
     @property
     @abstractmethod
-    def model(self) -> db_model_type:
+    def model(self) -> type[db_model_type]:
         raise NotImplementedError  # pragma: no cover
 
     @property
     @abstractmethod
-    def domain(self) -> domain_model_type:
+    def domain(self) -> type[domain_model_type]:
         raise NotImplementedError  # pragma: no cover
 
-    async def create(self, **kwargs: Any) -> domain_model_type:
-        model = self.model(**kwargs)
-        self.session.add(model)
-        return model
+    @property
+    @abstractmethod
+    def converter(self) -> type[converter_class]:
+        raise NotImplementedError  # pragma: no cover
+
+    async def create(self, domain_model: domain_model_type, update_id=False) -> domain_model_type:
+        db_model = self.converter.from_domain_to_model(domain_model)
+        self.session.add(db_model)
+        if update_id:
+            await self.session.flush()
+            domain_model.id = db_model.id
+        return domain_model
 
     async def get(self, id: int) -> domain_model_type:
         raise NotImplementedError
@@ -44,3 +57,4 @@ class AbstractRepository[db_model_type: type[DBModel], domain_model_type: type[D
 class SagaRepository(AbstractRepository):
     model = Saga
     domain = domain.Saga
+    converter = SagaConverter
--- tests/test_repositories.py
+++ tests/test_repositories.py
@@ -1,26 +1,27 @@
-from typing import Any
+from collections.abc import Callable
 from unittest.mock import Mock
 
 import pytest
-from mypy.typeanal import Callable
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from sagery import domain
 from sagery.repositories import AbstractRepository, SagaRepository
+from sagery.types import DomainModel
 
 
 @pytest.mark.parametrize(
-    ["klass", "dict_"],
+    ["klass", "object_"],
     [
-        (SagaRepository, {"id": 10**5, "name": "abc"}),
+        (SagaRepository, domain.Saga(id=10**5, name="abc", comment="comment", operators={}, queues=set())),
     ],
 )
 class TestRepository:
-    def test_init(self, klass: Callable[[AsyncSession], AbstractRepository], dict_: dict[str, Any]) -> None:
+    def test_init(self, klass: Callable[[AsyncSession], AbstractRepository], object_: DomainModel) -> None:
         session = Mock()
         repository = klass(session)
         assert repository.session is session
 
-    async def test_create(self, klass: Callable[[AsyncSession], AbstractRepository], dict_: dict[str, Any]) -> None:
+    async def test_create(self, klass: Callable[[AsyncSession], AbstractRepository], object_: DomainModel) -> None:
         session = Mock()
         repository = klass(session)
-        await repository.create(**dict_)
+        await repository.create(object_)
